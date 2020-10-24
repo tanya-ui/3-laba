ca00821a526d6f63cb51cb844537a46745232f12
tanya-ui <70968949+tanya-ui@users.noreply.github.com>
2020-10-24 22:27:54 +0600

    Add files via upload

---
+++ 3 Lab/3 Lab.cpp
@@ -0,0 +1,39 @@
+﻿// 3 Lab.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int massiv[10] = { 10,8,6,3,9,2,7,1,5,4 };
+	bool flag = true;
+	while (flag)
+	{
+		flag = false;
+		for (int i = 0; i < 9; i++)
+		{
+
+			if (massiv[i] > massiv[i + 1])
+			{
+				int elem = massiv[i];
+				massiv[i] = massiv[i + 1];
+				massiv[i + 1] = elem;
+				flag = true;
+
+			}
+			else if (massiv[i] < massiv[i - 1])
+			{
+				int elem = massiv[i];
+				massiv[i] = massiv[i - 1];
+				massiv[i - 1] = elem;
+				flag = true;
+			}
+
+		}
+	}
+	for (int i = 0; i < 10; i++)
+	{
+		cout << massiv[i] << endl;
+	}
+	return 0;
+}
